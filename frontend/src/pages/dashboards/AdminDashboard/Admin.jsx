c659cfeccd39bdd84ef3de47dcf35d44cd96de55
MonilMehta <monilmehta5@gmail.com>
2025-02-23 00:18:02 +0530

    Admin dashboard pt1

--- frontend/src/pages/dashboards/AdminDashboard/Admin.jsx
+++ frontend/src/pages/dashboards/AdminDashboard/Admin.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import AdminDashboard from "./pages/AdminDashboard";
 import Users from "./pages/Users";
+import UserProfile from "./pages/UserProfile";
 import Moderation from "./pages/Moderation";
 import Analytics from "./pages/Analytics";
 import { Routes, Route } from "react-router-dom";
@@ -12,6 +13,7 @@ const Admin = () => {
       <Routes>
         <Route path="/" element={<AdminDashboard />} />
         <Route path="users" element={<Users />} />
+        <Route path="users/:userId" element={<UserProfile />} />
         <Route path="moderation" element={<Moderation />} />
         <Route path="analytics" element={<Analytics />} />
       </Routes>
