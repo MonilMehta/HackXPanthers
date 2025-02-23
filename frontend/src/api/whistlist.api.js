1044300bbfa1012fe643c14e098d8c2307a9f9dd
ParamShukla007 <shuklaparam09022004@gmail.com>
2025-02-23 08:08:50 +0530

    added whistlist api

---
+++ frontend/src/api/whistlist.api.js
@@ -0,0 +1,5 @@
+import SERVER_API from "./server.api.js";
+
+export const createWishlist  = `${SERVER_API}/whistlist/createWishlist`;
+export const getAllWishlists = `${SERVER_API}/whistlist/getAllWishlists`;
+
